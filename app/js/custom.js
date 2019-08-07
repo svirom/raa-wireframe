dddc02856e2884fece7600b34492a7ec37a52611
svirom <svirom@yahoo.com>
2019-08-07 22:09:36 +0300

    main navigation mobile

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,1 +1,16 @@
-// Custom javascript
+$(document).ready(function() {
+
+  //mobile menu button
+  $('.nav-opener').on('click', function(e) {
+    e.preventDefault();
+
+    if ($(document).width() < 768) {
+      $(this).toggleClass('nav-active')
+        .next('.main-nav').find('.nav').slideToggle(300)
+        $(this).next('.main-nav').find('.search-form').slideToggle(300);
+        $(this).next('.main-nav').find('.member-login').slideToggle(300);
+    }
+  });
+
+
+});
