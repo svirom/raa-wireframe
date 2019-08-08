ea9e56f85b2f6bb07ef9a9c7d459c0eb174b57e6
svirom <svirom@yahoo.com>
2019-08-08 17:38:58 +0300

    header desktop + slider

--- app/js/custom.js
+++ app/js/custom.js
@@ -4,12 +4,31 @@ $(document).ready(function() {
   $('.nav-opener').on('click', function(e) {
     e.preventDefault();
 
-    if ($(document).width() < 768) {
+    if ($(document).width() < 1200) {
       $(this).toggleClass('nav-active')
-        .next('.main-nav').find('.nav').slideToggle(300)
-        $(this).next('.main-nav').find('.search-form').slideToggle(300);
-        $(this).next('.main-nav').find('.member-login').slideToggle(300);
+        .nextAll('.main-nav-wrapper').find('.nav').slideToggle(300);
+        $(this).nextAll('.main-nav-wrapper').find('.member-login').slideToggle(300);
+        $(this).nextAll('.search-form').slideToggle(300);
     }
+  });
+
+ 
+  $(window).resize(function() {
+    if ( $(document).width() >= 1200 ) {
+      $('.main-nav-wrapper').find('.nav').css('display', 'flex');
+      $('.main-nav-wrapper').find('.member-login').css('display', 'block');
+      $('.search-form').css('display', 'none');
+    } else {
+      $('.nav-opener').removeClass('nav-active');
+      $('.main-nav-wrapper').find('.nav').css('display', 'none');
+      $('.main-nav-wrapper').find('.member-login').css('display', 'none');
+      $('.search-form').css('display', 'none');
+    }
+  });
+
+  // slick slider
+  $('.main-slider').slick({
+    centerMode: true
   });
 
 
